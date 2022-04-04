a1e0123a905d424878a763e1d3be8ac512632ed0
ybra91 <102757205+ybra91@users.noreply.github.com>
2022-04-04 12:45:05 -0300

    Update chalengeop.py

--- chalengeop.py
+++ chalengeop.py
@@ -4,10 +4,7 @@ import requests
 def get_access_code():
     client_id = '1562860169862020'
     redirect_uri = 'https://challengeop.herokuapp.com/'
-    #scope = 'files.readwrite.all offline_access mail.readwrite'
-    url = 'https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id={}&redirect_uri={}
-    #url = """https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={}&scope={}
-     &response_type=code&redirect_uri={}'.format(client_id, redirect_uri)
+    url = 'https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id={}&redirect_uri={}&response_type=code&redirect_uri={}'.format(client_id, redirect_uri)
     return url
     
 print(get_access_code())
